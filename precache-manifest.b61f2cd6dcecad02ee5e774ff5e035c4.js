51dae25da6f9c7d673f7d4906060c0c0fe82d2e2
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-05-25 21:10:23 +0300

    Updates

---
+++ precache-manifest.b61f2cd6dcecad02ee5e774ff5e035c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c36998e23ddbb52095d1609d071aaba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ed4a30ddfb369004477",
+    "url": "./static/css/main.a50aad31.chunk.css"
+  },
+  {
+    "revision": "2ea62d86e75b944994f9",
+    "url": "./static/js/2.187eb7b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.187eb7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed4a30ddfb369004477",
+    "url": "./static/js/main.8fcdd961.chunk.js"
+  },
+  {
+    "revision": "13d57103233adbc95222",
+    "url": "./static/js/runtime-main.7c3e9311.js"
+  },
+  {
+    "revision": "bdce88035724b4def5b7f9cfe14eabc2",
+    "url": "./static/media/Search.bdce8803.svg"
+  },
+  {
+    "revision": "5ca5c1f58aa3bcefb2bf88439be8fe51",
+    "url": "./static/media/left.5ca5c1f5.svg"
+  },
+  {
+    "revision": "67be4a4a4d709745ad41a3578d953895",
+    "url": "./static/media/right.67be4a4a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.b61f2cd6dcecad02ee5e774ff5e035c4.js"
 );
 
 self.addEventListener('message', (event) => {
